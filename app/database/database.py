f219a95408381022f780b2209eae08f64329ab31
TechnoPanda <spote7992@gmail.com>
2026-08-26 16:14:35 +0100

    env local file committed

--- app/database/database.py
+++ app/database/database.py
@@ -1,15 +1,15 @@
 import os
+from pathlib import Path
 
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-load_dotenv()
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+load_dotenv(PROJECT_ROOT / ".env.local")
 
-URL_DATABASE = os.environ["DATABASE_URL"]
+DATABASE_URL = os.environ["DATABASE_URL"]
 
-engine = create_engine(URL_DATABASE)
-
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
